f0264d71c24154c2212ad8db1e563b15dfc068b1
Ivan Kohan <kohan3750@gmail.com>
2025-08-03 16:29:51 +0200

    change

--- src/components/UserMenu.jsx
+++ src/components/UserMenu.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { logoutUser } from "../services/api";
 
 const UserMenu = ({ email, setUser }) => {
--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ContactForm from "../components/ContactForm";
 import ContactList from "../components/ContactList";
 import Filter from "../components/Filter";
